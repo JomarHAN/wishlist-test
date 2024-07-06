c163a94468a0dde8caf6e3b1acb437a444653ac1
Jomar <jomarhan.ng@gmail.com>
2024-07-06 11:38:40 -0400

    removed unneeded tabled

--- prisma/migrations/20240706153636_wishlist_table/migration.sql
+++ prisma/migrations/20240706153636_wishlist_table/migration.sql
@@ -16,3 +16,18 @@ CREATE TABLE "Session" (
     "collaborator" BOOLEAN DEFAULT false,
     "emailVerified" BOOLEAN DEFAULT false
 );
+
+-- CreateTable
+CREATE TABLE "Settings" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "description" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Wishlist" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "customerId" TEXT,
+    "productId" TEXT,
+    "shop" TEXT
+);
