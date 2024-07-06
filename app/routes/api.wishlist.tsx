5586347cde444b46ef361cb3caa2b0f29dedc996
Jomar <jomarhan.ng@gmail.com>
2024-07-06 11:32:04 -0400

    main delete wishlist and get wishliist

--- app/routes/api.wishlist.tsx
+++ app/routes/api.wishlist.tsx
@@ -1,4 +1,4 @@
-import type { ActionFunctionArgs } from "@remix-run/node";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
 import db from "../db.server";
@@ -8,30 +8,49 @@ type TypeWishlist = {
   customerId: string;
   productId: string;
   shop: string;
+  _action: string;
 };
 
-export const loader = async () => {
-  return json({
-    status: "okay",
-    message: "Hello from heaven",
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const customerId = url.searchParams.get("customerId");
+  const productId = url.searchParams.get("productId");
+  const shop = url.searchParams.get("shop");
+
+  if (!customerId || !productId || !shop) {
+    alert("missing required datas");
+    return;
+  }
+
+  const wishlistFound = await db.wishlist.findMany({
+    where: {
+      customerId,
+      productId,
+      shop,
+    },
   });
+  const response = json({
+    message: "get wishlist successfull",
+    wishlisted: wishlistFound.length > 0 ? true : false,
+  });
+  return cors(request, response);
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const method = request.method;
   const formData = await request.formData();
   const dataObj = Object.fromEntries(formData);
-  const { customerId, productId, shop } = dataObj as TypeWishlist;
+  const { customerId, productId, shop, _action } = dataObj as TypeWishlist;
 
-  if (!customerId || !productId || !shop) {
+  if (!customerId || !productId || !shop || !_action) {
     return json({
       message: "Missing required arguements like: customerId, productId, shop",
     });
   }
 
-  switch (method) {
-    case "POST":
-      const wishlist = await db.wishlist.create({
+  switch (_action) {
+    case "CREATE":
+      await db.wishlist.create({
         data: {
           customerId,
           productId,
@@ -41,12 +60,28 @@ export const action = async ({ request }: ActionFunctionArgs) => {
       const response = json({
         message: "post success",
         method: method,
-        wishlist: wishlist,
+        wishlisted: true,
       });
       return cors(request, response);
 
     case "PATCH":
       return json({ message: "patch success", method: "patch" });
+
+    case "DELETE":
+      await db.wishlist.deleteMany({
+        where: {
+          customerId,
+          productId,
+          shop,
+        },
+      });
+
+      const resDelete = json({
+        message: "Deleted successful",
+        method: _action,
+        wishlisted: false,
+      });
+      return cors(request, resDelete);
 
     default:
       return json({ message: "everything else so far so good" });
